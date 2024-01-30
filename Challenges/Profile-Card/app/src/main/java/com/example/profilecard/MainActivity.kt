a09dd41c291fd4a3d5b02590ddd9444a43b17e2c
wallacesintra <wallvxwallace962@gmail.com>
2024-01-30 22:07:33 +0300

    Initial commit

--- Challenges/Profile-Card/app/src/main/java/com/example/profilecard/MainActivity.kt
+++ Challenges/Profile-Card/app/src/main/java/com/example/profilecard/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.example.profilecard
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.profilecard.ui.theme.ProfileCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ProfileCard()
                 }
             }
         }
@@ -30,17 +48,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ProfileCard(modifier: Modifier = Modifier){
+    Box {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(10.dp)
+                .width(180.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.femaleprofile),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .size(110.dp)
+                    .clip(CircleShape)
+
+            )
+            Text(
+                text = "Mercy Flow",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(5.dp)
+            )
+            Text(
+                text = "Lorem uugfs ihiaf ihiaf ojoaf ojoaf ojoaf ojoaf ojoaf ihiav",
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "View Profile")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ProfileCardTheme {
-        Greeting("Android")
+        ProfileCard()
     }
 }
