22a3c40fd0a22bc0dfbe4a9a8d2d6c2ecc7b17d9
wallacesintra <wallvxwallace962@gmail.com>
2024-02-01 04:05:32 +0300

    Animations added

--- Challenges/Profile-Card/app/src/main/java/com/example/profilecard/MainActivity.kt
+++ Challenges/Profile-Card/app/src/main/java/com/example/profilecard/MainActivity.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -102,11 +103,10 @@ fun ProfileCard(
             defaultElevation = 8.dp
         ),
         modifier = Modifier
-            .clickable { setIsClick(!isClicked)}
-//            .onFocusEvent { setIsClick(!isClicked) }
+            .clickable { setIsClick(!isClicked) }
             .animateContentSize(
                 animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioHighBouncy,
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
                     stiffness = Spring.StiffnessHigh
                 )
             )
@@ -180,7 +180,7 @@ fun ProfileCard(
                     .width(btnWidth)
             ) {
                 Text(
-                    text = "View Profile",
+                    text = stringResource(id = R.string.view_profile),
                     style = MaterialTheme.typography.labelSmall
                 )
             }
