682c6e01e67843e25752b88a3ea30b6b7d194215
wallacesintra <wallvxwallace962@gmail.com>
2024-02-01 03:06:01 +0300

    added animation to the view button

--- Challenges/Profile-Card/app/src/main/java/com/example/profilecard/MainActivity.kt
+++ Challenges/Profile-Card/app/src/main/java/com/example/profilecard/MainActivity.kt
@@ -2,19 +2,13 @@ package com.example.profilecard
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -25,12 +19,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Email
 import androidx.compose.material.icons.rounded.MailOutline
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -38,19 +29,14 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -84,7 +70,7 @@ fun ScreenLayout(){
             userData = UserData(
                 "Jane Doe",
                 "jane.doe@gmail.com",
-                "Freelancers for hire designed by Adrian Goia for Epic Coders",
+                "Freelancers for hire designed by Adrian for Epic Coders",
                 R.drawable.femaleprofile
             )
         )
@@ -92,7 +78,7 @@ fun ScreenLayout(){
             userData = UserData(
                 "John Doe",
                 "john.doe@gmail.com",
-                "Freelancers for hire designed by Adrian Goia for Epic Coders",
+                "Freelancers for hire designed by Adrian for Epic Coders",
                 R.drawable.maleprofile
             )
         )
@@ -101,7 +87,7 @@ fun ScreenLayout(){
 }
 @Composable
 fun ProfileCard(
-    modifier: Modifier = Modifier,
+//    modifier: Modifier = Modifier,
     userData: UserData,
 ){
     ElevatedCard (
@@ -156,26 +142,28 @@ fun ProfileCard(
                 textAlign = TextAlign.Center
             )
             Spacer(modifier = Modifier.height(20.dp))
-            var btnClicked by remember {
-                mutableStateOf(false)
-            }
 
-            val btnWidth by animateDpAsState(
+            val (isLoading, setIsLoading) = remember { mutableStateOf(false) }
 
-//                targetValue = if (btnClicked) 180.dp else 120.dp,
-                targetValue = 180.dp,
-                animationSpec = infiniteRepeatable(
-                    animation = tween(durationMillis = 3000),
-                    repeatMode = RepeatMode.Reverse
-                ),
-                label = "null"
-            )
             SideEffect {
-                Log.d("TAG", "clicked : $btnClicked")
+                Log.d("TAG", "clicked : $isLoading")
             }
+
+            val btnWidth = if (isLoading) 180.dp else 120.dp
             Button(
-                onClick = { btnClicked != btnClicked } ,
-                modifier = Modifier.width(btnWidth)
+                onClick = { setIsLoading(!isLoading) },
+
+                modifier = Modifier.animateContentSize(
+//                    animationSpec = tween(
+//                        durationMillis = 300,
+//                        easing = LinearOutSlowInEasing
+//                    )
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessHigh
+                    )
+                )
+                    .width(btnWidth)
             ) {
                 Text(
                     text = "View Profile",
@@ -194,7 +182,7 @@ fun GreetingPreview() {
             userData = UserData(
                 "Jane Doe",
                 "jane.doe@gmail.com",
-                "Freelancers for hire designed by Adrian Goia for Epic Coders",
+                "Freelancers for hire designed by Adrian for Epic Coders",
                 R.drawable.femaleprofile
             )
         )
